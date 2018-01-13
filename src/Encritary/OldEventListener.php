bb307c8aaf6525cf37a4eca7fd36ef57f4dccba0
Encritary <mrgenga537@gmail.com>
2018-01-13 11:10:40 +0500

    Create OldEventListener.php

---
+++ src/Encritary/OldEventListener.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * TransferSystem plugin for PocketMine-MP, spoons and Steadfast
+ * @author Encritary
+ */
+
+namespace Encritary;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\protocol\mcpe\TransferPacket;
+
+class SteadfastEventListener implements Listener{
+	
+	/** @var TransferSystem */
+	private $ts;
+
+	public function __construct(TransferSystem $ts){
+		$this->ts = $ts;
+	}
+
+	public function onPlayerLogin(PlayerLoginEvent $event){
+		if(!$this->ts->wasTransferedHere($event->getPlayer())){
+			$event->getPlayer()->transfer($this->ts->getIP(), 19132);
+		}
+	}
+
+	public function onDataPacketSend(DataPacketSendEvent $event){
+		if($event->getPacket() instanceof TransferPacket){
+			$this->ts->onTransferTo($event->getPlayer(), $event->getPacket()->ip, $event->getPacket()->port);
+		}
+	}
+
+}
